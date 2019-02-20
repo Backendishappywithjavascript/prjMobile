820126af821aa284386c3634a7e498c9b48f666c
Backendishappywithjavascript <wow@primasis.com>
2019-02-21 02:56:35 +0900

    Alterations: Made input with the Olá mundo! placeholder; Made a bit of JQuery with errors; Made a ul;

--- www/components/acesso.js
+++ www/components/acesso.js
@@ -1,9 +1,8 @@
 
-$(function(){
-  alert('Alerta!');
-  $(li).on('click',() => {
-    alert("You've found one 'li'!");
-    if($(this).is('.shiny'))
+$(() => {
+  $('li').on('click',() => {
+    alert("You've found a 'li'!");
+    if($(this).is(".shiny"))
     {
       alert("and You've found the shiny one!");
     }
