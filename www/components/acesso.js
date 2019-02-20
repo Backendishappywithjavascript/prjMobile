6c0050f51660d6f279951975447aff6cb061bb48
Backendishappywithjavascript <wow@primasis.com>
2019-02-21 03:36:03 +0900

    Fixed jquery error on shiny alert

--- www/components/acesso.js
+++ www/components/acesso.js
@@ -1,10 +1,11 @@
 
 $(() => {
+  
   $('li').on('click',() => {
     alert("You've found a 'li'!");
-    if($(this).is(".shiny"))
-    {
-      alert("and You've found the shiny one!");
-    }
+  })
+
+  $('li.shiny').on('click',() => {
+    alert("and is a shiny one!")
   })
 });
